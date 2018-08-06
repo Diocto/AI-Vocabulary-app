88f73c77e2d3af1f4afea87f8f2330e25e7fe0ed
duhee <duhee74@naver.com>
2018-08-06 16:52:03 +0900

    Add word test activity layout
    add word test faculty layout, and renaming some ids.

---
+++ app/src/main/java/org/andoidtown/ai_vocabulary/WordTestActivity.java
@@ -0,0 +1,13 @@
+package org.andoidtown.ai_vocabulary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WordTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_word_test);
+    }
+}
